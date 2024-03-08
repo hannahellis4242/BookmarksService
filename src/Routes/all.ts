179f470dca55f26cb8c266703efd527c91525dc5
Hannah Ellis <hannahellis4242@gmail.com>
2024-03-08 08:39:49 +0000

    dealing with the case that a link has no tags, was previously 404ing (#9)

--- src/Routes/all.ts
+++ src/Routes/all.ts
@@ -4,35 +4,40 @@ import LabelService from "../Service/LabelService";
 import TagService from "../Service/TagService";
 import handleError, { Errors } from "./utils/handleErrors";
 import Bookmark from "../Model/Bookmark";
+import ServiceErrors from "../Service/ServiceErrors";
 
 const all = (service: LabelService & LinkService & TagService) =>
-  Router()
-    .get("/", (_, res) =>
-      service
-        .allLinks()
-        .then((linkIds) =>
-          Promise.all(
-            linkIds.map((linkId) =>
+  Router().get("/", (_, res) =>
+    service
+      .allLinks()
+      .then((linkIds) =>
+        Promise.all(
+          linkIds.map((linkId) =>
+            service.getLink(linkId).then(({ link }) =>
               service
-                .getLink(linkId)
-                .then(({ link }) =>
-                  service.findTagsWithLink(linkId).then((tags) =>
-                    Promise.all(
-                      tags.map((tag) =>
-                        service
-                          .getTagLabelID(tag)
-                          .then((labelID) => service.getLabel(labelID))
-                      )
+                .findTagsWithLink(linkId)
+                .catch((e) =>
+                  e === ServiceErrors.NotFound
+                    ? Promise.resolve([])
+                    : Promise.reject(e)
+                )
+                .then((tags) =>
+                  Promise.all(
+                    tags.map((tag) =>
+                      service
+                        .getTagLabelID(tag)
+                        .then((labelID) => service.getLabel(labelID))
                     )
-                      .then((labels) => labels.map(({ label }) => label))
-                      .then((labels) => ({ link, labels }))
                   )
+                    .then((labels) => labels.map(({ label }) => label))
+                    .then((labels) => ({ link, labels }))
                 )
             )
           )
         )
-        .then((bookmarks: Bookmark[]) => res.json(bookmarks))
-        .catch(handleError(res))
-    )
+      )
+      .then((bookmarks: Bookmark[]) => res.json(bookmarks))
+      .catch(handleError(res))
+  );
 
 export default all;
